a58915ca9c30e9e10425dca25334f642b1608c88
joao.davi <joao.davi@sankhya.com.br>
2022-06-04 15:58:16 -0300

    melhorias

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import AppContent from './AppContent'
 import AppSidebar from './AppSidebar'
 import DocsExample from './DocsExample'
+import AppHeader from './AppHeader'
 
 export {
   AppContent,
   AppSidebar,
   DocsExample,
+  AppHeader
 }
