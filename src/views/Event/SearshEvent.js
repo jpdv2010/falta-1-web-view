5e4bf6e4862ac8e302ba0b47477b84a52b03dd2e
joao.davi <joao.davi@sankhya.com.br>
2022-11-18 06:50:17 -0300

    adicionando filtro por partidas

--- src/views/Event/SearshEvent.js
+++ src/views/Event/SearshEvent.js
@@ -1,7 +1,26 @@
-import { CRow, CCol, CCard, CCardBody, CCardTitle, CCardText, CButton } from "@coreui/react"
+import React from 'react'
+import {
+    CRow, CCol, CCard, CCardBody, CCardTitle, CCardText, CButton, CForm,
+    CFormInput,
+    CFormSelect,
+    CFormLabel
+} from "@coreui/react"
 import _events from '../../_events';
+import {
+    MuiPickersUtilsProvider,
+    KeyboardDatePicker,
+} from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
+import CIcon from '@coreui/icons-react';
+import { cilFilter } from '@coreui/icons'
 
 const SearshEvent = () => {
+    const [filter, setFilter] = React.useState({});
+    const [filtered, setFiltered] = React.useState(false);
+    const [sport, setSport] = React.useState(undefined);
+    const [city, setCity] = React.useState(undefined);
+    const [schedule, setSchedule] = React.useState(undefined);
+
     const getEvents = () => {
         return _events;
     }
@@ -11,37 +30,106 @@ const SearshEvent = () => {
         return size;
     }
 
+    const handleSubmit = () => {
+        let filter = {
+            sport: sport,
+            city: city,
+            schedule: schedule
+        };
+        setFilter(filter);
+
+        setFiltered(true);
+    }
+
+    const gotoFilter = () => {
+        setFilter({});
+        setFiltered(false);
+    }
+
     return (
-        <CRow>
-            {getEvents().map((item, index) => (
-                <CCol>
-                <CCard className="w-100">
+        <>
+            {filtered ?
+                <></> :
                 <CCardBody>
-                    <CCardTitle>{item.name}</CCardTitle>
-                    <CRow>
-                        <CCol>
-                            <CCardText>
-                                Vagas restantes: {getVagasRestantes(item)}
-                            </CCardText>
-                            <CCardText>
-                                Data:  {item.date}
-                            </CCardText>
+                    <CForm onSubmit={(event) => handleSubmit(event)} className="row g-3">
+                        <CCol md={3}>
+                            <CFormLabel htmlFor="inputPassword4">Data</CFormLabel>
+                            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                                <KeyboardDatePicker
+                                    disableToolbar
+                                    variant="inline"
+                                    format="dd/MM/yyyy"
+                                    margin="normal"
+                                    id="date-picker-inline"
+                                    value={schedule}
+                                    onChange={(value) => { setSchedule(value) }}
+                                    KeyboardButtonProps={{
+                                        'aria-label': 'change date',
+                                    }}
+                                />
+                            </MuiPickersUtilsProvider>
                         </CCol>
-                        <CCol>
-                            <CCardText>
-                                Cidade: {item.address.city}
-                            </CCardText>
-                            <CCardText>
-                                Bairro:  {item.address.district}
-                            </CCardText>
+                        <CCol md={3}>
+                            <CFormLabel htmlFor="inputState">Esporte</CFormLabel>
+                            <CFormSelect id="inputGroupSelect01" onChange={(event) => { setSport(event.target?.value) }} value={sport} >
+                                <option>Esporte...</option>
+                                <option value="FUTEBOL">Futebol</option>
+                                <option value="VOLEI">Vôlei</option>
+                                <option value="BEACH TENNIS">Beach Tenis</option>
+                                <option value="BASQUETE">Basquete</option>
+                                <option value="HANDBALL">Handball</option>
+                                <option value="TÊNIS">Tênis</option>
+                            </CFormSelect>
                         </CCol>
-                    </CRow>
-                    <CButton href="#">Juntar-se ao Evento</CButton>
+                        <CCol md={6}>
+                            <CFormLabel htmlFor="inputEmail4">Cidade</CFormLabel>
+                            <CFormInput placeholder="Cidade" autoComplete="city" type="text" onChange={(event) => { setCity(event.target?.value) }} value={city} />
+                        </CCol>
+                        <CCol xs={12}>
+                            <CButton type="submit">Filtrar</CButton>
+                        </CCol>
+                    </CForm>
                 </CCardBody>
-                </CCard>
-                </CCol>
-            ))}
-        </CRow>
+            }
+            <CRow>
+                {filtered? 
+                <CRow>
+                    <CCol md={4}>
+                        <CButton onClick={event => gotoFilter()}><CIcon icon={cilFilter} size="lg" /></CButton>
+                    </CCol>
+                </CRow> : <></>
+                }
+                <div style={{paddingTop: '5px'}}/>
+                {getEvents().map((item, index) => (
+                    <CCol>
+                        <CCard className="w-100">
+                            <CCardBody>
+                                <CCardTitle>{item.name}</CCardTitle>
+                                <CRow>
+                                    <CCol>
+                                        <CCardText>
+                                            Vagas restantes: {getVagasRestantes(item)}
+                                        </CCardText>
+                                        <CCardText>
+                                            Data:  {item.date}
+                                        </CCardText>
+                                    </CCol>
+                                    <CCol>
+                                        <CCardText>
+                                            Cidade: {item.address.city}
+                                        </CCardText>
+                                        <CCardText>
+                                            Bairro:  {item.address.district}
+                                        </CCardText>
+                                    </CCol>
+                                </CRow>
+                                <CButton href="#">Juntar-se ao Evento</CButton>
+                            </CCardBody>
+                        </CCard>
+                    </CCol>
+                ))}
+            </CRow>
+        </>
     )
 }
 
